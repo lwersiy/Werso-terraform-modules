e4df5a3350bc23a61551f63b74a206ad4fe51940
lwersiy <petitwerso2003@yahoo.com>
2023-08-26 22:08:11 -0400

    backend directed to s3 added

--- root-modules/child-modules/ec2/main.tf
+++ root-modules/child-modules/ec2/main.tf
@@ -10,3 +10,4 @@ resource "aws_instance" "web" {
     environment = var.env
   }
 }
+
--- root-modules/child-modules/vpc/output.tf
+++ root-modules/child-modules/vpc/output.tf
@@ -1,3 +1,7 @@
 output "publick_subnets_ids" {
   value = module.vpc.public_subnets
 }
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
